6bd20c8ecab9560623bc1647fd78a076d656a6cb
yuto.inoue <inoue.yuto0513@gmail.com>
2022-10-06 15:11:51 +0900

    ページネーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -34,12 +34,20 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        }catch(NumberFormatException e) {}
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).setFirstResult(15 * (page - 1)).setMaxResults(15).getResultList();
+
+        long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class).getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);
+        request.setAttribute("messages_count", messages_count);
+        request.setAttribute("page", page);
 
         //フラッシュメッセージがセッションスコープにセットされていたら
         //リクエストスコープに保存する
